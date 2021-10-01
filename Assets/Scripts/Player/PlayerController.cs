2d86c68d25a8c1bee3b387d7c8a5f28dc42f9f80
Batarse1 <00014419@uca.edu.sv>
2021-10-01 12:06:31 -0600

    :tada: Begin a Unity project
    Start the Unity project for testing, add some assets, add some objects, add some animations and add some mechanics.
    
    Since it's essential to start learning the basics with Unity in a non-theorical way.
    
    authored-by: Fernando González <Batarse1>

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private bool move;
+    private float horizontalInput;
+    private float verticalInput;
+    private float lastHorizontalInput;
+    private float lastVerticalInput;
+
+    public Animator animator;
+
+    //Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("Move", move);
+
+        animator.SetFloat("X", lastHorizontalInput);
+        animator.SetFloat("Y", lastVerticalInput);
+
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        move = true;
+        if (horizontalInput != 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+            lastHorizontalInput = horizontalInput;
+            lastVerticalInput = 0;
+        }
+        else if (verticalInput != 0)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+            lastVerticalInput = verticalInput;
+            lastHorizontalInput = 0;
+        }
+        else
+        {
+            move = false;
+        }
+    }
+}
